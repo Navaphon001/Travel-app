242fbcde5b2cdaa9edfcc186690a2803259040a5
Navaphon001 <nutnavaphon@hotmail.com>
2025-07-13 14:37:38 +0700

    fix login

--- app/routers/login_router.py
+++ app/routers/login_router.py
@@ -23,7 +23,7 @@ def get_db():
 def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
     user = db.query(User).filter(User.username == form_data.username).first()
 
-    if not user or not verify_password(form_data.password, user.password):
+    if not user or not verify_password(form_data.password, user.hashed_password):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Incorrect username or password",
